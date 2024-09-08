22329b24095454181e4f7f9e9855df143e7aa868
wangyuandong <chong2vv@gmail.com>
2024-09-08 12:01:50 +0800

    updateCTMediator

--- YDMediator/Classes/CTMediator/CTMediator.h
+++ YDMediator/Classes/CTMediator/CTMediator.h
@@ -8,14 +8,20 @@
 
 #import <Foundation/Foundation.h>
 
+extern NSString * _Nonnull const kCTMediatorParamsKeySwiftTargetModuleName;
 
 @interface CTMediator : NSObject
 
-+ (instancetype)sharedInstance;
++ (instancetype _Nonnull)sharedInstance;
 
 // 远程App调用入口
-- (id)performActionWithUrl:(NSURL *)url completion:(void(^)(NSDictionary *info))completion;
+- (id _Nullable)performActionWithUrl:(NSURL * _Nullable)url completion:(void(^_Nullable)(NSDictionary * _Nullable info))completion;
 // 本地组件调用入口
-- (id)performTarget:(NSString *)targetName action:(NSString *)actionName params:(NSDictionary *)params;
-
+- (id _Nullable )performTarget:(NSString * _Nullable)targetName action:(NSString * _Nullable)actionName params:(NSDictionary * _Nullable)params shouldCacheTarget:(BOOL)shouldCacheTarget;
+- (void)releaseCachedTargetWithFullTargetName:(NSString * _Nullable)fullTargetName;
+// 检查 是否有引源码
+- (BOOL)check:(NSString * _Nullable)targetName moduleName:(NSString * _Nullable)moduleName;
 @end
+  
+// 简化调用单例的函数
+CTMediator* _Nonnull CT(void);
